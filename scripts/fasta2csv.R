3ad6449090e826521f06d0dfc8897eb34ab99cc5
James Lawson <the.hierarchy@gmail.com>
2016-11-03 12:15:17 +1100

    calcualted protein MWs

--- scripts/fasta2csv.R
+++ scripts/fasta2csv.R
@@ -2,6 +2,8 @@ install.packages('seqinr')
 
 require(seqinr)
 require(readr)
+require(dplyr)
+require(stringr)
 
 blah <- read.fasta('data/large_files/Egrandis_Eglobulus_chloroplast_Myrtales_At_mt_cRAP_160405.fasta', seqtype='AA', as.string=TRUE, strip.desc=TRUE)
 
@@ -9,11 +11,13 @@ EGrandis_AA_seqs <- data.frame(do.call(rbind, blah))
 
 write.csv(EGrandis_AA_seqs, 'data/large_files/Egrandis_Eglobulus_chloroplast_Myrtales_At_mt_cRAP_160405.csv')
 
+# had to remove proteins with | at the end (e.g. human, bov etc.)
+
 EGrandis_AA_seqs <- read_csv('data/large_files/Egrandis_Eglobulus_chloroplast_Myrtales_At_mt_cRAP_160405.csv')
 
 ion_areas <- read_csv('data/large_files/D14_ion_areas_new_ion_library.csv')
 
-
 D14_euc_AA_seqs <- EGrandis_AA_seqs[EGrandis_AA_seqs$Protein %in% ion_areas$Protein,]
 
 write_csv(D14_euc_AA_seqs, 'data/large_files/Egrandis_Eglobulus_chloroplast_Myrtales_At_mt_cRAP_160405_D14eucSubset.csv')
+
---
+++ scripts/protein_MW.R
@@ -0,0 +1,11 @@
+# protein molecular weights #
+
+require(Peptides)
+require(readr)
+
+D14_euc_AA_seqs <- read_csv('data/large_files/Egrandis_Eglobulus_chloroplast_Myrtales_At_mt_cRAP_160405_D14eucSubset.csv')
+
+D14_euc_AA_seqs$MW <- apply(D14_euc_AA_seqs[,2],1, mw)
+
+write_csv(D14_euc_AA_seqs, 'data/D14_protein_MW.csv')
+
