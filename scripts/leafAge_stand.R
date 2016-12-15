a2b55d7de0499e390eaf6131d792263294d45007
James Lawson <the.hierarchy@gmail.com>
2016-12-15 15:00:27 +1100

    leaf age stand 2

--- scripts/leafAge_stand.R
+++ scripts/leafAge_stand.R
@@ -1,4 +1,4 @@
-#source('scripts/transformations.R')
+source('scripts/transformations.R')
 
 replicates <- read_csv('output/replicates.csv')
 replicates <- replicates[replicates$sample %in% protein_stand_D14_age$sample,]
@@ -7,38 +7,27 @@ data <- merge(protein_stand_D14_age, climate_locs)
 data <- merge(data, replicates, by = c('sample', 'Latitude', 'Longitude', 'leaf_age', 'site_revised'))
 data <- data[!duplicated(data$sample),]
 
+data[data$ID == 'corgum_47',]$ID <- 'corgum_46'
 
-data$leaf_age <- factor(data$leaf_age, levels = c('new', 'mid', 'old'))
-
-#boxplot(total_protein ~ leaf_age, data)
-
-
-total_protein <- data %>% select(total_protein, leaf_age, biological_rep, ID) %>%
-  mutate(total_protein_stand = NA) %>%
-  filter(!ID %in% 'melpal_106')
-
-total_protein[total_protein$ID == 'corgum_47',]$ID <- 'corgum_46'
-
-corcit <- data.frame(total_protein = NA, leaf_age = 'mid', biological_rep = 2, ID = 'corcit_43')
-cortes <- data.frame(total_protein = NA, leaf_age = 'old', biological_rep = 3, ID = 'cortes_51')
-eucmed <- data.frame(total_protein = NA, leaf_age = 'new', biological_rep = 3, ID = 'eucmed_65')
-eucdel <- data.frame(total_protein = NA, leaf_age = 'old', biological_rep = 3, ID = 'eucdel_58')
-eucglo <- data.frame(total_protein = NA, leaf_age = 'new', biological_rep = 2, ID = 'eucglo_63')
-eucpun <- data.frame(total_protein = NA, leaf_age = 'mid', biological_rep = 3, ID = 'eucpun_71')
-eucspa <- data.frame(total_protein = NA, leaf_age = 'old', biological_rep = 2, ID = 'eucspa_75')
-eucten_new <- data.frame(total_protein = NA, leaf_age = 'new', biological_rep = 2, ID = 'eucten_77')
-eucten_old <- data.frame(total_protein = NA, leaf_age = 'old', biological_rep = 3, ID = 'eucten_77')
-corexi <- data.frame(total_protein = NA, leaf_age = 'new', biological_rep = 3, ID = 'corexi_45')
-corpoc <- data.frame(total_protein = NA, leaf_age = 'old', biological_rep = 2, ID = 'corpoc_49')
-eucdum <- data.frame(total_protein = NA, leaf_age = 'new', biological_rep = 1, ID = 'eucdum_60')
-euchae <- data.frame(total_protein = NA, leaf_age = 'mid', biological_rep = 1, ID = 'euchae_64')
-eucrub <- data.frame(total_protein = NA, leaf_age = 'new', biological_rep = 3, ID = 'eucrub_73')
-#eucter <- data.frame(total_protein = NA, leaf_age = 'new', biological_rep = 3, ID = 'eucrub_73')
+corcit <- data.frame(data = NA, leaf_age = 'mid', biological_rep = 2, ID = 'corcit_43')
+cortes <- data.frame(data = NA, leaf_age = 'old', biological_rep = 3, ID = 'cortes_51')
+eucmed <- data.frame(data = NA, leaf_age = 'new', biological_rep = 3, ID = 'eucmed_65')
+eucdel <- data.frame(data = NA, leaf_age = 'old', biological_rep = 3, ID = 'eucdel_58')
+eucglo <- data.frame(data = NA, leaf_age = 'new', biological_rep = 2, ID = 'eucglo_63')
+eucpun <- data.frame(data = NA, leaf_age = 'mid', biological_rep = 3, ID = 'eucpun_71')
+eucspa <- data.frame(data = NA, leaf_age = 'old', biological_rep = 2, ID = 'eucspa_75')
+eucten_new <- data.frame(data = NA, leaf_age = 'new', biological_rep = 2, ID = 'eucten_77')
+eucten_old <- data.frame(data = NA, leaf_age = 'old', biological_rep = 3, ID = 'eucten_77')
+corexi <- data.frame(data = NA, leaf_age = 'new', biological_rep = 3, ID = 'corexi_45')
+corpoc <- data.frame(data = NA, leaf_age = 'old', biological_rep = 2, ID = 'corpoc_49')
+eucdum <- data.frame(data = NA, leaf_age = 'new', biological_rep = 1, ID = 'eucdum_60')
+euchae <- data.frame(data = NA, leaf_age = 'mid', biological_rep = 1, ID = 'euchae_64')
+eucrub <- data.frame(data = NA, leaf_age = 'new', biological_rep = 3, ID = 'eucrub_73')
 
 #eucspa doesnt have biological rep 3 (fixed) - and YG118 is missing from protein_stand_D14_age (!) this needs further investigation
 #biological rep needs to be checked for added lines
 
-total_protein <- total_protein %>% 
+data <- data %>% 
   bind_rows(corcit) %>% 
   bind_rows(cortes) %>% 
   bind_rows(eucmed) %>%
@@ -53,6 +42,14 @@ total_protein <- total_protein %>%
   bind_rows(eucdum) %>%
   bind_rows(euchae) %>%
   bind_rows(eucrub)
+
+rm(corcit,cortes,eucmed,eucdel,eucglo,eucpun,eucspa,eucten_new,eucten_old,corexi,corpoc,eucdum,euchae,eucrub)
+
+# TOTAL PROTEIN
+
+total_protein <- data %>% select(total_protein, leaf_age, biological_rep, ID) %>%
+  mutate(total_protein_stand = NA) %>%
+  filter(!ID %in% 'melpal_106')
 
 total_protein <- total_protein[order(total_protein[,4], total_protein[,2], total_protein[,3]),]
 
@@ -72,3 +69,15 @@ check <- total_protein %>%
   filter(leaf_age == 'new') %>% 
   group_by(ID, leaf_age) %>%
   summarise(mean_new = mean(total_protein_stand, na.rm=TRUE))
+
+# PLOT
+
+data$leaf_age <- factor(data$leaf_age, levels = c('new', 'mid', 'old'))
+
+boxplot(total_protein ~ leaf_age, data)
+
+total_protein$leaf_age <- factor(total_protein$leaf_age, levels = c('new', 'mid', 'old'))
+
+boxplot(total_protein_stand ~ leaf_age, total_protein)
+
+summary(aov(total_protein_stand ~ leaf_age, subset(total_protein, leaf_age != 'new')))
