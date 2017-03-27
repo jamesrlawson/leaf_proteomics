4d6f36bb4daadb3c547321c0979f358d066cadc9
James Lawson <the.hierarchy@gmail.com>
2017-03-28 10:43:09 +1100

    working on introduction, added some figs

--- scripts/transformations.R
+++ scripts/transformations.R
@@ -120,16 +120,16 @@ licor <- licor[licor$Cond > 0.05,]
 
 leaf_CN <- read_csv('data/leaf_CNP/leaf_CN.csv')
 leaf_CN <- leaf_CN[leaf_CN$sample %in% climate_locs$sample,]
-#climate_locs <- merge(leaf_CN, climate_locs, all.y=TRUE, by = 'sample')
+climate_locs <- merge(leaf_CN, climate_locs, all.y=TRUE, by = 'sample')
 
-#climate_locs$N_per_area <- climate_locs$N * 10 * climate_locs$LMA_g_per_m2
+climate_locs$N_per_area <- climate_locs$N * 10 * climate_locs$LMA_g_per_m2
 
 
 leaf_P <- read_csv('data/leaf_CNP/leaf_P.csv')
 leaf_P <- leaf_P[leaf_P$sample %in% climate_locs$sample,]
-#climate_locs <- merge(leaf_P, climate_locs, all.y=TRUE, by = 'sample')
+climate_locs <- merge(leaf_P, climate_locs, all.y=TRUE, by = 'sample')
 
-#climate_locs$P_per_area <- climate_locs$P * 10 * climate_locs$LMA_g_per_m2
+climate_locs$P_per_area <- climate_locs$P * 10 * climate_locs$LMA_g_per_m2
 
 # leaf age 
 
