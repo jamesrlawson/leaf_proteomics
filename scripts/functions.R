37b5b1b759a1163bb5b4bc038f8c7a02e95c435e
James Lawson <the.hierarchy@gmail.com>
2018-04-23 17:22:13 +1000

    working on a results markdown doc

--- scripts/functions.R
+++ scripts/functions.R
@@ -779,182 +779,11 @@ effect_size <- function(proportion, depvar, logx) {
 
 
 
-aggplot_save_combined_2 <- function(proportion, indepvar, logx = FALSE, indepvarType, labs, outDir, fileType = 'tiff', goldenRatio = FALSE) {
+aggplot_save_combined_4 <- function(proportion, indepvar, logx = FALSE, indepvarType, labs,
+                                    save=TRUE, outDir, fileType = 'tiff', 
+                                    output_list = FALSE, goldenRatio = FALSE, yaxis = FALSE) {
   
-  # data - the output of scripts/prep_data.R or sources/prep_data_mg_per_mm2.R (contains all climate/env variables and protein amounts) 
-  # depvar - the dependent variable (e.g. 'Photosystems')  
-  # indepvar - the independent variable (e.g. 'prec')  # logx - should x be logged? (x axis to be printed as log scaled)
-  # proportion - does the y axis label describe proportions or mg/mm2 ?
-  # indepvarType - standard, gap, leafrad or LMA (to use appropriate horizontal error bars - standard uses none)  
-  # labs - should contain:   
-  #   a pretty form of the dependent variable (labs[1])  
-  #   the x axis label (labs[2]) 
-  # outdir - directory to output to 
-  # golden ratio - plot dimensions golden ratio? 
-  
-  require(lazyeval) 
-  if(proportion) { # source data conditional on proportion 
-    source('scripts/prep_data.R') 
-  } else { 
-    source('scripts/prep_data_mg_per_mm2.R') 
-  } 
-  
-  if(proportion) { # set labels conditional on proportion
-    type = '_proportion_' 
-  } else { 
-    type = '_mg-per-m2_' 
-  } 
-  
-  
-  dep_means <- group_by(data, ID) %>% 
-    dplyr::summarise(other_calv = mean(calvin_cycle, na.rm=TRUE) - mean(Rubisco, na.rm=TRUE),     
-                     rub = mean(Rubisco, na.rm=TRUE),    
-                     phot = mean(Photosystems, na.rm=TRUE)) %>%   
-    gather(key = 'funccat', value = 'protein_mean', -ID)
-  
-  dep_means <- group_by(data, ID) %>% 
-    dplyr::summarise(other_calv = SE(calvin_cycle) - SE(Rubisco),     
-                     rub = SE(Rubisco), 
-                     phot = SE(Photosystems_min_LHC)) %>%
-    gather(key = 'funccat', value = 'protein_SE', -ID) %>%  
-    full_join(dep_means, by = c('ID', 'funccat')) %>%  
-    full_join(data, by = 'ID') %>% 
-    distinct(ID, funccat, .keep_all=TRUE) 
-  
-  if(logx) { # get significance TRUE/FALSE for model, depending on if logx or not
-    sig_othercalv <- summary(lm(dep_means[dep_means$funccat %in% 'other_calv',]$protein_mean ~ log10(dep_means[dep_means$funccat %in% 'other_calv',][[indepvar]])))$coefficients[,4][2] < 0.05  
-    sig_rub <- summary(lm(dep_means[dep_means$funccat %in% 'rub',]$protein_mean ~ log10(dep_means[dep_means$funccat %in% 'rub',][[indepvar]])))$coefficients[,4][2] < 0.05 
-    sig_phot <- summary(lm(dep_means[dep_means$funccat %in% 'phot',]$protein_mean ~ log10(dep_means[dep_means$funccat %in% 'phot',][[indepvar]])))$coefficients[,4][2] < 0.05 
-  } else {
-    sig_othercalv <- summary(lm(dep_means[dep_means$funccat %in% 'other_calv',]$protein_mean ~ dep_means[dep_means$funccat %in% 'other_calv',][[indepvar]]))$coefficients[,4][2] < 0.05 
-    sig_rub <- summary(lm(dep_means[dep_means$funccat %in% 'rub',]$protein_mean ~ dep_means[dep_means$funccat %in% 'rub',][[indepvar]]))$coefficients[,4][2] < 0.05
-    sig_phot <- summary(lm(dep_means[dep_means$funccat %in% 'phot',]$protein_mean ~ dep_means[dep_means$funccat %in% 'phot',][[indepvar]]))$coefficients[,4][2] < 0.05
-  }
-  
-  dep_means$sig <- NA 
-  dep_means[dep_means$funccat == 'other_calv',]$sig <- sig_othercalv                                
-  dep_means[dep_means$funccat == 'rub',]$sig <- sig_rub
-  dep_means[dep_means$funccat == 'phot',]$sig <- sig_phot 
-  
-  # directory to save to
-  mainDir <- getwd() 
-  
-  if (!file.exists(outDir)){ 
-    dir.create(file.path(mainDir, outDir), showWarnings = FALSE, recursive = TRUE)
-  }
-  
-  if(goldenRatio) { 
-    Width = 6*1.618 
-  } else {
-    Width = 6.5 
-  } 
-  
-  
-  
-  if(fileType == 'svg'){
-    svg(paste(outDir, '/',indepvar, type, '.svg', sep = ""), height = 6, width = Width)
-  } else {
-    if(fileType == 'tiff') {
-      tiff(paste(outDir, '/', indepvar, type, '.tiff', sep = ""), height = 6, width = Width, units = 'in', res = 300)
-    }
-  }
-  
-  # plot object
-  
-  p <- ggplot(data = dep_means, aes(y = protein_mean, x = dep_means[[indepvar]])) + geom_point(aes(colour = funccat, shape = funccat),size = 1.5)
-  
-  p <- p + geom_smooth(data = dep_means[dep_means$sig == TRUE,], aes(y = protein_mean, x = get(indepvar), colour = funccat), method = 'lm', se = F, size = 0.5)
-  
-  p <- p + scale_colour_manual(values = c('blue','forestgreen','red'))
-  
-  if(proportion) {
-    p <- p + xlab(labs[2]) +  ylab(paste(labs[1], ' (proportion)', sep = ""))
-  } else {
-    p <- p + xlab(labs[2]) +  ylab(bquote(.(labs[1]) ~ '(mg / ' ~ m^{2} ~ ')'))
-    
-  }
-  
-  #p <- p + expand_limits(y=0,x=0)
-  
-  p <- p + theme_classic()
-  p <- p + theme(legend.title=element_blank(),
-                 legend.position="bottom",
-                 axis.line = element_line(colour = "black"),
-                 text = element_text(size = 18))
-  p <- p + expand_limits(y=0)
-  
-  
-  number_ticks <- function(n) {function(limits) pretty(limits, n)}
-  
-  if(logx) {
-    
-    errorbar_width <- (log10(max(data[[indepvar]], na.rm=TRUE)) - log10(min(data[[indepvar]], na.rm=TRUE))) / 50
-    p <- p + geom_errorbar(aes(colour = funccat, ymin = protein_mean - protein_SE, ymax = protein_mean + protein_SE), width = 0, alpha = 0.3, size = 0.5)
-    
-    if(indepvarType == 'gap') {
-      
-      p <- p + geom_errorbarh(aes(xmin = gap_mean - gap_SE, xmax = gap_mean + gap_SE, colour = funccat), alpha = 0.3, size = 0.5)
-      
-    } else { if(indepvarType == 'leafrad') {
-      
-      p <- p + geom_errorbarh(aes(xmin = leafrad_mean - leafrad_SE, xmax = leafrad_mean + leafrad_SE, colour = funccat), alpha = 0.3, size = 0.5)
-      
-    }
-    }
-    
-    p <- p + scale_x_continuous(breaks=scales::pretty_breaks(n=10),trans='log10')
-    
-  } else {
-    
-    errorbar_width <- (max(data[[indepvar]], na.rm=TRUE) - min(data[[indepvar]], na.rm=TRUE)) / 50
-    p <- p + geom_errorbar(aes(colour = funccat, ymin = protein_mean - protein_SE, ymax = protein_mean + protein_SE), width = 0, alpha = 0.3, size = 0.5)
-    
-    if(indepvarType == 'gap') {
-      
-      p <- p + geom_errorbarh(aes(xmin = gap_mean - gap_SE, xmax = gap_mean + gap_SE, colour = funccat), alpha = 0.3, size = 0.5)
-      
-    } else { if(indepvarType == 'leafrad') {
-      
-      p <- p + geom_errorbarh(aes(xmin = leafrad_mean - leafrad_SE, xmax = leafrad_mean + leafrad_SE, colour = funccat), alpha = 0.3, size = 0.5)
-      
-      
-    } else { if(indepvarType == 'LMA') {
-      
-      p <- p + geom_errorbarh(aes(xmin = LMA_mean - LMA_SE, xmax = LMA_mean + LMA_SE, colour = funccat), alpha = 0.3, size = 0.5)
-      
-    } else { if(indepvarType == 'Narea') {
-      
-      p <- p + geom_errorbarh(aes(xmin = Narea_mean - Narea_SE, xmax = Narea_mean + Narea_SE, colour = funccat), alpha = 0.3, size = 0.5)
-      
-    } else { if(indepvarType == 'total_protein') {
-      
-      p <- p + geom_errorbarh(aes(xmin = total_protein_mean - total_protein_SE, xmax = total_protein_mean + total_protein_SE, colour = funccat), alpha = 0.3, size = 0.5)
-      
-    }
-      
-    }
-      
-    }
-      
-    }
-      
-    }
-    
-    p <- p + scale_x_continuous(breaks=scales::pretty_breaks(n=10))
-    
-  }
-  
-  print(p)
-  
-  dev.off()
-  
-}
-
-
-
-
-
-aggplot_save_combined_3 <- function(proportion, indepvar, logx = FALSE, indepvarType, labs, outDir, fileType = 'tiff', goldenRatio = FALSE, yaxis = FALSE) {
+  outlist <- list()
   
   # data - the output of scripts/prep_data.R or sources/prep_data_mg_per_mm2.R (contains all climate/env variables and protein amounts) 
   # depvar - the dependent variable (e.g. 'Photosystems')  
@@ -995,16 +824,25 @@ aggplot_save_combined_3 <- function(proportion, indepvar, logx = FALSE, indepvar
     distinct(ID, funccat, .keep_all=TRUE) 
   
   if(logx) { # get significance TRUE/FALSE for model, depending on if logx or not
-    sig_calv <- summary(lm(dep_means[dep_means$funccat %in% 'calv',]$protein_mean ~ log10(dep_means[dep_means$funccat %in% 'calv',][[indepvar]])))$coefficients[,4][2] < 0.05  
-    sig_phot <- summary(lm(dep_means[dep_means$funccat %in% 'phot',]$protein_mean ~ log10(dep_means[dep_means$funccat %in% 'phot',][[indepvar]])))$coefficients[,4][2] < 0.05 
+    model_calv <- lm(dep_means[dep_means$funccat %in% 'calv',]$protein_mean ~ log10(dep_means[dep_means$funccat %in% 'calv',][[indepvar]]))
+    model_phot <- lm(dep_means[dep_means$funccat %in% 'phot',]$protein_mean ~ log10(dep_means[dep_means$funccat %in% 'phot',][[indepvar]]))
+    sig_calv <- summary(model_calv)$coefficients[,4][2] < 0.05
+    sig_phot <- summary(model_phot)$coefficients[,4][2] < 0.05
   } else {
-    sig_calv <- summary(lm(dep_means[dep_means$funccat %in% 'calv',]$protein_mean ~ dep_means[dep_means$funccat %in% 'calv',][[indepvar]]))$coefficients[,4][2] < 0.05 
-    sig_phot <- summary(lm(dep_means[dep_means$funccat %in% 'phot',]$protein_mean ~ dep_means[dep_means$funccat %in% 'phot',][[indepvar]]))$coefficients[,4][2] < 0.05
+    model_calv <- lm(dep_means[dep_means$funccat %in% 'calv',]$protein_mean ~ dep_means[dep_means$funccat %in% 'calv',][[indepvar]])
+    model_phot <- lm(dep_means[dep_means$funccat %in% 'phot',]$protein_mean ~ dep_means[dep_means$funccat %in% 'phot',][[indepvar]])
+    sig_calv <- summary(model_calv)$coefficients[,4][2] < 0.05
+    sig_phot <- summary(model_phot)$coefficients[,4][2] < 0.05
   }
   
   dep_means$sig <- NA 
   dep_means[dep_means$funccat == 'calv',]$sig <- sig_calv                                
   dep_means[dep_means$funccat == 'phot',]$sig <- sig_phot 
+  
+  # output lm summaries to list
+  
+  outlist[[1]] <- model_calv
+  outlist[[2]] <- model_phot
   
   # directory to save to
   mainDir <- getwd() 
@@ -1020,13 +858,16 @@ aggplot_save_combined_3 <- function(proportion, indepvar, logx = FALSE, indepvar
   } 
   
   
-  
-  if(fileType == 'svg'){
-    svg(paste(outDir, '/',indepvar, type, '.svg', sep = ""), height = 6, width = Width)
-  } else {
-    if(fileType == 'tiff') {
-      tiff(paste(outDir, '/', indepvar, type, '.tiff', sep = ""), height = 6, width = Width, units = 'in', res = 300)
+  if(save) {
+    
+    if(fileType == 'svg'){
+      svg(paste(outDir, '/',indepvar, type, '.svg', sep = ""), height = 6, width = Width)
+    } else {
+      if(fileType == 'tiff') {
+        tiff(paste(outDir, '/', indepvar, type, '.tiff', sep = ""), height = 6, width = Width, units = 'in', res = 300)
+      }
     }
+    
   }
   
   # plot object
@@ -1118,6 +959,72 @@ aggplot_save_combined_3 <- function(proportion, indepvar, logx = FALSE, indepvar
   
   print(p)
   
-  dev.off()
+  outlist[[3]] <- p
+  
+  if(save) {
+    dev.off()
+  } 
+  
+  if(output_list){return(outlist)}
+  
+}
+
+# generate bespoke lm outputs
+# takes a list where element 1 is the lm for calvin cycle vs whatever indepvar, element 2 is the lm for photosystems vs indepvar
+lmSummary <- function(l, logx) {
+  
+  # logx = FALSE
+  
+  lm_summary <- glance(summary(l[[1]])) %>%
+    rbind(., glance(summary(l[[2]])))
+  lm_summary$df[1] <-  summary(l[[1]])$df[2]
+  lm_summary$df[2] <-  summary(l[[2]])$df[2]
+  
+  lm_summary <- cbind(model = c('light independent rxns',
+                                'photosystems'),
+                      lm_summary)
+  lm_summary$adj.r.squared <- NULL
+  
+  # light independent rxns model
+  
+  mod <- l[[1]]
+  
+  if(logx) {
+    start = as.numeric(coef(mod)[1]) + as.numeric(coef(mod)[2]) * min(mod$model[2])
+    end = as.numeric(coef(mod)[1]) + as.numeric(coef(mod)[2]) * max(mod$model[2])
+  } else { 
+    if(!logx) {
+      start = as.numeric(coef(mod)[1]) + as.numeric(coef(mod)[2]) * min(mod$model[2])
+      end = as.numeric(coef(mod)[1]) + as.numeric(coef(mod)[2]) * max(mod$model[2])
+    }
+  }
+  
+  lm_summary$fit_start <- NA
+  lm_summary$fit_end <- NA
+  lm_summary$fit_change <- NA
+  
+  lm_summary$fit_start[1] <- start
+  lm_summary$fit_end[1] <- end
+  lm_summary$fit_change[1] <- (end - start)/start
+  
+  # photosystems model
+  
+  mod <- l[[2]]
+  
+  if(logx) {
+    start = as.numeric(coef(mod)[1]) + as.numeric(coef(mod)[2]) * min(mod$model[2])
+    end = as.numeric(coef(mod)[1]) + as.numeric(coef(mod)[2]) * max(mod$model[2])
+  } else { 
+    if(!logx) {
+      start = as.numeric(coef(mod)[1]) + as.numeric(coef(mod)[2]) * min(mod$model[2])
+      end = as.numeric(coef(mod)[1]) + as.numeric(coef(mod)[2]) * max(mod$model[2])
+    }
+  }
+  
+  lm_summary$fit_start[2] <- start
+  lm_summary$fit_end[2] <- end
+  lm_summary$fit_change[2] <- (end - start)/start
+  
+  return(lm_summary)
   
 }
---
+++ scripts/results_rmd.R
@@ -0,0 +1,6 @@
+source("scripts/functions.R")
+source("scripts/transformations.R")
+source("scripts/prep_data_mg_per_mm2.R")
+library("rmarkdown")
+
+render('rmd/euc_MS_results.Rmd')
